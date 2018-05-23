872bd623ebdbc3b9bc861de01382b6345659e555
Fernando Wibowo <30991688+fwibowo88@users.noreply.github.com>
2018-05-23 13:54:27 +0700

    Lanjut di PC

---
+++ PETER PROJECT/src/ConHandler.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fwadmin
+ */
+public class ConHandler {
+    BufferedReader in;
+    Socket s;
+    
+    public ChatHandler(Socket a)
+    {
+        try
+        {
+        }
+        catch
+    
+    }
+}
